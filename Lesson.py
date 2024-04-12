ddcc470973e94490431bb1f76edf390fe3f684b9
user <andrey.gromov.36912@gmail.com>
2024-04-12 14:33:47 +0300

    Завдання 2 модуль 11

--- Lesson.py
+++ Lesson.py
@@ -1,32 +1,8 @@
-#Завдання 2
-#Створіть дескриптор для атрибуту, що зберігає
-#розмір файлу. Додайте можливість зберігати розмір
-#файлу в байтах, але представляти його у зручному для
-#читання форматі (наприклад, КБ або МБ).
-class FileSizeDescriptor:
-    def __init__(self, size_bytes=0):
-        self.size_bytes = size_bytes
+# Завдання 2
+# Створіть дескриптор для атрибуту, що зберігає
+# розмір файлу. Додайте можливість зберігати розмір
+# файлу в байтах, але представляти його у зручному для
+# читання форматі (наприклад, КБ або МБ).
 
-    def __get__(self, instance, owner):
-        return self.size_bytes
 
-    def __set__(self, instance, value):
-        if value < 0:
-            raise ValueError("Розмір файлу не може бути від'ємним")
-        self.size_bytes = value
 
-    def formatted_size(self):
-        if self.size_bytes < 1024:
-            return f"{self.size_bytes} B"
-        elif self.size_bytes < 1024 * 1024:
-            return f"{self.size_bytes / 1024:.2f} KB"
-        else:
-            return f"{self.size_bytes / (1024 * 1024):.2f} MB"
-
-class File:
-    size = FileSizeDescriptor()
-
-file = File()
-file.size = 2048
-print("Розмір файлу:", file.size)
-print("Форматований розмір файлу:", file.size.formatted_size())  # Виведе: 2.00 KB
