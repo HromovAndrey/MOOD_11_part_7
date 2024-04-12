f53de66d067a907a7669d62ee7224269b9745bd6
user <andrey.gromov.36912@gmail.com>
2024-04-12 21:40:06 +0300

    Завдання 5 модуль 11

--- Lesson.py
+++ Lesson.py
@@ -1,46 +1,35 @@
-# Завдання 4
-# Створіть клас для представлення користувача з
-# атрибутами: ім'я та вік. Додайте властивості для
-# валідації віку користувача. Наприклад, вік повинен
-# бути у межах від 0 до 120.
-class User:
-    def __init__(self, name, age):
-        self.name = name
-        self._age = age
+# Завдання 5
+#  Створіть клас Multiplier, який при ініціалізації
+# отримує множник. Забезпечте можливість викликати
+# цей об'єкт з аргументом та повертати множене
+# значення.
 
-    @property
-    def age(self):
-        return self._age
+class Multiplier:
+    def __init__(self, factor):
+        self.factor = factor
 
-    @age.setter
-    def age(self, value):
-        if not isinstance(value, int):
-            raise TypeError("Age must be an integer")
-        if value < 0 or value > 120:
-            raise ValueError("Age must be between 0 and 120")
-        self._age = value
+    def __call__(self, value):
+        new_value = value - (value * self.factor)
+        return  new_value
+
+multiplier = Multiplier(5)
+
+result = multiplier(10)
+print("Result:", result)
+
+result = multiplier(7)
+print("Result:", result)
 
 
-user = User("John", 25)
-print("Name:", user.name)
-print("Age:", user.age)
-user.age = 30
-print("New Age:", user.age)
-
-user.age = 150
-
-# s_strok = " "
-# s_list = []
-# s_number = 0
-# a = [5, 3, "hello", [3,4], "world", [5], 10.5]
-# for i in a:
-#     if isinstance(i,str):
-#         s_strok = s_strok + i
-#     elif isinstance(i, list):
-#         s_list = s_list + i
-#     elif isinstance((i, (int, float))):
-#         s_number = s_number + i
+# class DiscountCalculator:
+#     def __init__(self, discount):
+#         self._discount = discount/100
 #
-# print(s_strok)
-# print(s_list)
-# print(s_number)
+#
+#
+#     def __call__(self, price):
+#         new_price = price - (price * self._discount)
+#         #new_price = price * (1 - self._discount)
+#         return new_price
+# calculator = DiscountCalculator(20)
+# print(calculator(50))
