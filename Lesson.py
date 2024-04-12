b4471cf1e59e9f2cb498c0b721c9d872dd250747
user <andrey.gromov.36912@gmail.com>
2024-04-12 21:33:17 +0300

    Завдання 4 модуль 11

--- Lesson.py
+++ Lesson.py
@@ -1,92 +1,46 @@
-# Завдання 3
-# Створіть клас для представлення відомостей про
-# замовлення. Забезпечте, щоб номер замовлення був
-# тільки для читання за допомогою керованих атрибутів,
-# але його можна було переглядати.
-class Order:
-    def __init__(self, order_number, details):
-        self._order_number = order_number
-        self._details = details
+# Завдання 4
+# Створіть клас для представлення користувача з
+# атрибутами: ім'я та вік. Додайте властивості для
+# валідації віку користувача. Наприклад, вік повинен
+# бути у межах від 0 до 120.
+class User:
+    def __init__(self, name, age):
+        self.name = name
+        self._age = age
 
     @property
-    def order_number(self):
-        return self._order_number
+    def age(self):
+        return self._age
+
+    @age.setter
+    def age(self, value):
+        if not isinstance(value, int):
+            raise TypeError("Age must be an integer")
+        if value < 0 or value > 120:
+            raise ValueError("Age must be between 0 and 120")
+        self._age = value
 
 
-    def details(self):
-        return self._details
+user = User("John", 25)
+print("Name:", user.name)
+print("Age:", user.age)
+user.age = 30
+print("New Age:", user.age)
 
-    @order_number.getter
-    def order_number(self):
-        return self._order_number
+user.age = 150
 
-    @details.getter
-    def detalist(self):
-        return self._details
-
-
-
-order = Order("12345", "245")
-print(order.order_number)
-print(order.detalist)
-
-
-
-# class ImageResizer:
-#     def __init__(self, width, height, max_widht=1000, max_height=1000):
-#         self._width = width
-#         self._height = height
-#         self._max_widht = max_widht
-#         self._max_height = max_height
+# s_strok = " "
+# s_list = []
+# s_number = 0
+# a = [5, 3, "hello", [3,4], "world", [5], 10.5]
+# for i in a:
+#     if isinstance(i,str):
+#         s_strok = s_strok + i
+#     elif isinstance(i, list):
+#         s_list = s_list + i
+#     elif isinstance((i, (int, float))):
+#         s_number = s_number + i
 #
-#     @property
-#     def width(self):
-#         print("propetry from width")
-#         return self._width
-#     @width.getter
-#     def width(self):
-#         print("with.getter")
-#         return self._width
-#
-#     @width.setter
-#     def width(self, value):
-#         print("with.setter")
-#         if value <= self._max_widht:
-#            self._width = value
-#         else:
-#             raise ValueError("bad value")
-#
-#     @property
-#     def height(self):
-#         print("propetry from height")
-#         return self._height
-#
-#     @height.getter
-#     def height(self):
-#         print("height getter")
-#         return self._height
-#
-#     @height.setter
-#     def height(self, value):
-#         print("height setter")
-#         if value <= self._max_height:
-#             self._height = value
-#         else:
-#             raise ValueError("bad value")
-#     @height.deleter
-#     def height(self):
-#         print("hight from deleter")
-#         def self._height
-#         self._height = None #замовчування
-#
-# resizer = ImageResizer(0, 0, 1200, 1600)
-# resizer.width = 100
-# print(resizer.width)
-# # resizer.height = 300
-# # print(resizer.height)
-# del resizer.height
-
-#чому в мене не працює deleter??
-
-
-
+# print(s_strok)
+# print(s_list)
+# print(s_number)
